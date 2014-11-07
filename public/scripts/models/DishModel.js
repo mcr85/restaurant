bf9cc215dca4186932392dd2eef003202efda58c
mcr85 <mcr5@o2.pl>
2014-11-07 15:24:02 +0100

    adding karma; adding DishModel and test for it

---
+++ public/scripts/models/DishModel.js
@@ -0,0 +1,1 @@
+var DishModel = Backbone.Model.extend({});
---
+++ public/tests/models/DishModel_test.js
@@ -0,0 +1,9 @@
+describe('DishModel suite', function() {
+
+    var dishModel = new DishModel();
+
+    it('Can be instantiated', function() {
+        expect(DishModel).to.be.ok();
+    });
+
+});
